ba3f8f4d7f1a8a3a664b672c8180ea7fdc15be07
rmcgregor1990 <rmcgregor1990@gmail.com>
2014-07-23 00:31:49 +0100

    change visitor to a class

--- jsonparse.py
+++ jsonparse.py
@@ -3,30 +3,30 @@ import json
 x = '{"type":"SerialFrame","children":[{"type":"ParallelFrame","children":[{"type":"SerialFrame","children":[{"type":"TerminalState","moduleName":"LED1","name":"state1","args":[{"name":"Current","value":"24"},{"name":"Time","value":""},{"name":"another","value":""}]},{"type":"TerminalState","moduleName":"LED1","name":"state1","args":[{"name":"Current","value":""},{"name":"Time","value":""},{"name":"another","value":""}]}]},{"type":"SerialFrame","children":[{"type":"TerminalState","moduleName":"LED1","name":"state1","args":[{"name":"Current","value":""},{"name":"Time","value":""},{"name":"another","value":""}]}]}]},{"type":"TerminalState","moduleName":"LED1","name":"state1","args":[{"name":"Current","value":""},{"name":"Time","value":""},{"name":"another","value":""}]}]}'
 
 
-def visit(node):
-    return globals()['visit_'+node['type']](node)
+class Visitor():
+    def visit(self, node):
+        return self.__getattribute__('visit_'+node['type'])(node)   
 
-def visit_SerialFrame(node):
-    ans = "("
-    for child in node['children']:
-        ans += visit(child)+"+"
-    return ans[:-1] +")"
+    def visit_SerialFrame(self, node):
+        ans = "("
+        for child in node['children']:
+            ans += self.visit(child)+"+"
+        return ans[:-1] +")"
 
 
-def visit_ParallelFrame(node):
-    ans = "("
-    for child in node['children']:
-        ans += visit(child)+"&"
-    return ans[:-1] +")" 
+    def visit_ParallelFrame(self, node):
+        ans = "("
+        for child in node['children']:
+            ans += self.visit(child)+"&"
+        return ans[:-1] +")" 
 
-def visit_TerminalState(node):
-    ans = node['moduleName']+"."+node['name']+"("
-    for agr in node['args']:
-        ans += agr['value'] + ","
-    return ans[:-1] +")"
+    def visit_TerminalState(self, node):
+        ans = node['moduleName']+"."+node['name']+"("
+        for agr in node['args']:
+            ans += agr['value'] + ","
+        return ans[:-1] +")"
 
 if __name__ == '__main__':
     root_node = json.loads(x)
-    print(visit(root_node))
-
-(((LED1.state1(24,,)+LED1.state1(,,))&(LED1.state1(,,)))+LED1.state1(,,))
+    vistor = Visitor();
+    print(vistor.visit(root_node))
